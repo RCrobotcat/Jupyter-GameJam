28d67e070d218a285353ae7a76153d206d6ad505
RCrobotcat <136684976+RCrobotcat@users.noreply.github.com>
2024-11-25 21:06:53 +0800

    Added player movement logics

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    NavMeshAgent agent;
+    float horizontal;
+    // float vertical;
+    float stopDistance;
+    SpriteRenderer playerSprite;
+    Animator animator;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        playerSprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        // vertical = Input.GetAxis("Vertical");
+
+        Vector3 inputDirection = new Vector3(horizontal, 0, 0);
+        if (inputDirection != Vector3.zero)
+        {
+            Vector3 CamRelativeMove = ConvertToCameraSpace(inputDirection);
+            MovePlayer(CamRelativeMove);
+        }
+
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+
+        if (horizontal < 0)
+        {
+            playerSprite.flipX = false;
+        }
+        else if (horizontal > 0)
+        {
+            playerSprite.flipX = true;
+        }
+    }
+
+    public void MovePlayer(Vector3 inputDirection)
+    {
+        Vector3 targetPosition = transform.position + inputDirection;
+        MoveToTarget(targetPosition);
+    }
+
+    public void MoveToTarget(Vector3 target)
+    {
+        // StopAllCoroutines()
+        agent.stoppingDistance = stopDistance;
+        agent.destination = target;
+    }
+
+    private Vector3 ConvertToCameraSpace(Vector3 vectorToRotate)
+    {
+        Vector3 camForward = Camera.main.transform.forward;
+        Vector3 camRight = Camera.main.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+
+        Vector3 CamForwardZProduct = vectorToRotate.z * camForward;
+        Vector3 CamRightXProduct = vectorToRotate.x * camRight;
+
+        Vector3 vectorRotateToCameraSpace = CamForwardZProduct + CamRightXProduct;
+        return vectorRotateToCameraSpace;
+    }
+}
